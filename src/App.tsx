6d400d741a8599c4f1ed581a4f88143fff13f22d
Dima Cherednyk <dicherednyk@gmail.com>
2023-02-04 18:50:12 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
 export const App: React.FC = () => {
+  const [string, setString] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+
+  const findUniqueLetter = (text: string) => {
+    const words = text.split(' ');
+    const deleteNewLine = words.map(x => x.replace(/[\n]/g, ' '));
+    const deleteSpaces = deleteNewLine.filter(word => word !== '');
+    const wordsToLowerCase = deleteSpaces.map(word => word.toLowerCase());
+    const uniqueLetterFromWord: string[] = [];
+
+    wordsToLowerCase.forEach(word => {
+      const x = word.split('');
+
+      x.find(t => {
+        if (word.indexOf(t) === word.lastIndexOf(t)) {
+          return uniqueLetterFromWord.push(t);
+        }
+
+        return null;
+      });
+    });
+
+    const resultLetter = uniqueLetterFromWord.filter(letter => {
+      if (uniqueLetterFromWord.indexOf(letter) === uniqueLetterFromWord.lastIndexOf(letter)) {
+        return letter;
+      }
+
+      return null;
+    });
+
+    return setResult(resultLetter[0] || 'No unique letter!');
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="app">
+      <h3>Find unique letter</h3>
+      <div className="app__main">
+        <input value={string} onChange={(e) => setString(e.currentTarget.value)} type="text" />
+
+        <button
+          onClick={() => setString('')}
+          type="button"
+          disabled={!string}
+        >
+          Delete all
+        </button>
+      </div>
+
+      <button
+        onClick={() => findUniqueLetter(string)}
+        type="button"
+        disabled={!string}
+      >
+        Find unique letter
+      </button>
+
+      <p
+        className={classNames(
+          'app__result', {
+            'app__result--hidden': !result,
+          },
+        )}
+      >
+        {`Result: ${result}`}
+      </p>
     </div>
   );
 };
